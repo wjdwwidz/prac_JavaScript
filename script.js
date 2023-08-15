549f3d4b464e0a946af26e8fa78f3fc7e4ee3d2e
Jeonghye Lee <wjdwwidz@gmail.com>
2023-08-15 14:02:27 +0900

    test commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+console.log('hello javascript');
+
+var age =25;
+let year =2023;
+const point =250;
+const today = '광복절' //문자
+
+const pass = true;
+
+if (pass){
+    console.log('hello')
+}
+
+//Number
+//String
+//Boolean
+
+//Null , Undefined, Object
+console.log(age);
+console.log(year);
+console.log(250);
